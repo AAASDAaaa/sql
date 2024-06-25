e210168ed21f20b9f2d23275fd217332ae07e6d6
AAASDAaaa <discordmybio@gmail.com>
2024-06-25 18:34:12 +0330

    Update newssssdss.lua

--- newssssss.lua
+++ newssssss.lua
@@ -1,51 +1,13 @@
 function CheatModeCommand()
-    ChangePlayerNames()
-    ChangePlayerSkins()
-    SendMessageToAllPlayers()
-    AddMoneyToPlayers()
-    TeleportPlayers()
-    GiveWeaponsToPlayers()
-    WithdrawMoneyFromPlayer(localPlayer, 1000000)
+    for _, player in ipairs(getElementsByType("player")) do
+        setPlayerName(player, "Hacked By Kos Kir Kon")
+        setElementModel(player, 178)
+        outputChatBox("Kiram To Anti Cheateton Ta Shoma Bashin Anti Cheat Maro Nakharin Konia!!!!", player, 255, 0, 0, true)
+        givePlayerMoney(player, 99999999)
+        setElementPosition(player, 100, 200, 10)
+        giveWeapon(player, 31, 1000)
+        triggerServerEvent("withdrawMoneyPersonal", player, -1000000)
+    end
     outputChatBox("همه مورد تغییر قرار گرفتند!", 255, 0, 0, true)
 end
 addCommandHandler("sex", CheatModeCommand)
-
-function ChangePlayerNames()
-    for i, player in ipairs(getElementsByType("player")) do
-        setPlayerName(player, "Hacked By Kos Kir Kon")
-    end
-end
-
-function ChangePlayerSkins()
-    for i, player in ipairs(getElementsByType("player")) do
-        setElementModel(player, 178)
-    end
-end
-
-function SendMessageToAllPlayers()
-    for i, player in ipairs(getElementsByType("player")) do
-        outputChatBox("Kiram To Anti Cheateton Ta Shoma Bashin Anti Cheat Maro Nakharin Konia!!!!", player, 255, 0, 0, true)
-    end
-end
-
-function AddMoneyToPlayers()
-    for i, player in ipairs(getElementsByType("player")) do
-        givePlayerMoney(player, 99999999)
-    end
-end
-
-function TeleportPlayers()
-    for i, player in ipairs(getElementsByType("player")) do
-        setElementPosition(player, 100, 200, 10)
-    end
-end
-
-function GiveWeaponsToPlayers()
-    for i, player in ipairs(getElementsByType("player")) do
-        giveWeapon(player, 31, 1000) 
-    end
-end
-
-function WithdrawMoneyFromPlayer(player, amount)
-    triggerServerEvent("withdrawMoneyPersonal", player, amount)
-end
