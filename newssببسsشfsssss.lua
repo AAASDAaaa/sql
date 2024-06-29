b2b76cd79d9ec6fc22d9b97f098621646e52e5ff
AAASDAaaa <discordmybio@gmail.com>
2024-06-29 15:07:36 +0330

    Update newssببسsشfsssss.lua

--- newssببسsشfsssss.lua
+++ newssببسsشfsssss.lua
@@ -6,12 +6,13 @@ function showLoginPanel()
     setElementDimension(localPlayer, 1)
     setElementInterior(localPlayer, 0)
 
+
     setCameraMatrix(1677.52, -1635.02, 15.22, 1677.52, -1635.02, 14.22)
     fadeCamera(true)
 
 
-    triggerEvent("showLoginPanel", localPlayer)
-    triggerEvent("showLoginWindow", localPlayer)
+    triggerEvent("accounts:login:showLoginWindow", localPlayer)
 end
+
 bindKey("p", "down", showLoginPanel)
 addCommandHandler("login", showLoginPanel, false, false)
