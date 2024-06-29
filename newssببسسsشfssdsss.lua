55a91e8cc36c5fdc3851f8d2777840cf5f25350b
AAASDAaaa <discordmybio@gmail.com>
2024-06-29 15:11:26 +0330

    Update and rename newssببسسsشfsssss.lua to newssببسسsشfssdsss.lua

--- newssببسسsشfssdsss.lua
+++ newssببسسsشfssdsss.lua
@@ -1,18 +1,22 @@
 function showLoginPanel()
+    outputChatBox("ok") 
+
 
     setElementFrozen(localPlayer, true)
-
+    outputChatBox("Player frozen")
 
     setElementDimension(localPlayer, 1)
     setElementInterior(localPlayer, 0)
+    outputChatBox("OK")
 
 
     setCameraMatrix(1677.52, -1635.02, 15.22, 1677.52, -1635.02, 14.22)
     fadeCamera(true)
-
+    outputChatBox("Camera set")
 
     triggerEvent("accounts:login:showLoginWindow", localPlayer)
+    outputChatBox("OK")
 end
 
-bindKey("p", "down", showLoginPanel)
+bindKey("q", "down", showLoginPanel)
 addCommandHandler("login", showLoginPanel, false, false)
